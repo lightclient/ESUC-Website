2f3fef7822e789df1df0e8d604d58581e955f67e
MATT GARNETT <MATT@MATTGARNETT.COM>
2016-02-05 21:39:14 -0800

    Fixing permalinks issue

--- reset_permalinks.php
+++ reset_permalinks.php
@@ -1,7 +1,5 @@
 <?php
-    // set permalinks on heroku
     global $wp_rewrite;
-    $wp_rewrite->set_permalink_structure( '/%year%/%monthnum%/%postname%/' );
-    $wp_rewrite->flush_rules();
+    $wp_rewrite->flush_rewrite_rules();
     echo "Success";
 ?>
